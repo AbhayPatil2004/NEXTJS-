1743c0fcb4936b9ebff3e441fa53064252f891a1
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-09-02 16:03:52 +0530

    error handing

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,27 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useTheme } from "@/context/themeProvider";
 import error from "./error";
+import { useState } from "react";
 
 export default function Home() {
-  const name = "Abhay"
 
- 
-    if (name == "Abhay") {
-      throw new Error("My error");
-    }
-  
-    console.log(error)
-  
+  const name = "Abhay"
+  const [ fruits , setFruits] = useState(['mango' , 'appple' , 'banana' , 'jamun' , 'jasmine'])
 
   return (
+  <div>
+    Home Page {name}
+    <button
+      onClick={setFruits(null)}
+    >
+      Click
+    </button>
     <div>
-      Home Page {name}
+      {fruits.map((fruit) => (
+        <p key={fruit}>{fruit}</p>
+      ))}
     </div>
-  );
+  </div>
+);
+
 }
